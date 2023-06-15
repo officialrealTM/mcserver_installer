55a345d8cbb6365da065f52dff46f8febaa08085
BuildTools <unconfigured@null.spigotmc.org>
2023-06-16 00:08:18 +0200

    - Removed unnessary character which caused errors

--- mcserver_installer.sh
+++ mcserver_installer.sh
@@ -1,4 +1,4 @@
-d#!/bin/bash
+#!/bin/bash
 
 ###################################################################
 #Script Name	: MCServerInstaller                                                                                          
