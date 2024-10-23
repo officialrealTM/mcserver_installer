f8c4985d21c399190eadc70462d679b7ac0a6391
BuildTools <unconfigured@null.spigotmc.org>
2024-10-23 16:15:53 +0200

    - Added support for Minecraft 1.21.3

--- mcserver_installer.sh
+++ mcserver_installer.sh
@@ -84,7 +84,7 @@ function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)"
 function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$ver"; }
 
 minVer=1.7
-maxVer=1.21.2
+maxVer=1.21.3
 
 if version_lt $ver $minVer; then
     not_supported
@@ -3638,7 +3638,7 @@ distro_check () {
 }
 
 ## Script Version
-scriptversion="14.1"
+scriptversion="14.2"
 ##
 
 ## Latest Version
