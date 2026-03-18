480c2ee26421a073c6a9950578b9c1d105e2df00
root <root@vmi3044786.contaboserver.net>
2026-03-18 21:37:22 +0100

    - Updated mcurlgrabber.py to successfully parse snapshot variants and default to Java 8 for legacy snapshots lacking manifest schema details.

--- mcurlgrabber.py
+++ mcurlgrabber.py
@@ -1,14 +1,13 @@
 import sys
 import urllib.request
 import json
-from packaging import version
 
 def find_version_info(ver):
     versions_raw = urllib.request.urlopen("https://launchermeta.mojang.com/mc/game/version_manifest.json").read()
     versions = json.loads(versions_raw)["versions"]
 
-    for version_info in filter(lambda x: x["type"] == "release", versions):
-        if version.parse(version_info["id"]) == ver:
+    for version_info in filter(lambda x: x["type"] in ["release", "snapshot"], versions):
+        if version_info["id"] == ver:
             version_info_raw = urllib.request.urlopen(version_info["url"]).read()
             return json.loads(version_info_raw)
 
@@ -17,25 +16,31 @@ def find_version_info(ver):
 
 def get_java_version(ver):
     version_info = find_version_info(ver)
-    java_version = version_info["javaVersion"]["majorVersion"]
+    if "javaVersion" in version_info:
+        java_version = version_info["javaVersion"]["majorVersion"]
+    else:
+        java_version = 8
     print(java_version)
 
 
 def get_server_url(ver):
     version_info = find_version_info(ver)
-    server_url = version_info["downloads"]["server"]["url"]
-    print(server_url)
+    if "downloads" in version_info and "server" in version_info["downloads"]:
+        server_url = version_info["downloads"]["server"]["url"]
+        print(server_url)
+    else:
+        sys.exit(1)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         raise Exception("Invalid arguments")
 
-    v = version.parse(sys.argv[2])
-    if not isinstance(v, version.Version):
-        sys.exit(1)
+    v = sys.argv[2]
 
     if sys.argv[1] == "server-url":
         get_server_url(v)
     elif sys.argv[1] == "java-version":
         get_java_version(v)
+    else:
+        sys.exit(1)
